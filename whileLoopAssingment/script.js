c4a9d7702a3e631666402b8d52b4a818719b1ced
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-04-30 18:13:59 +0530

    sum of numbers from 10 to 5 using while loop

--- whileLoopAssingment/script.js
+++ whileLoopAssingment/script.js
@@ -15,3 +15,22 @@ while(j >= 1)
     console.log(j);
     j--;
 }
+
+//quetion number 3
+
+let k = 2;
+while(k < 11)
+{
+    console.log(k);
+}
+
+//quetion number 4
+
+let num = 10;
+let sum = 0;
+while(num >= 5)
+{
+    sum = sum + num;
+    num--;
+}
+console.log("sum = " , sum);
