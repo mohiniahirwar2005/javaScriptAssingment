8f2adc82e183f865d7db6bfb5e25200dbdfeaff9
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-05-01 19:18:45 +0530

    javaScript Function and Methods

---
+++ jsMethods/script.js
@@ -0,0 +1,8 @@
+let a = 20 , b = 30;
+
+function sum(m,n){
+    let k = m+n
+    console.log(k);
+}
+
+//sum(a,b)
---
+++ jsMethods/script2.js
@@ -0,0 +1,22 @@
+//const abc = function(){
+  //  console.log("Hello World");
+//};
+//console.log(abc);
+
+//abc();
+
+//abc();
+
+//function abc (){
+  //  console.log("Hello World");
+//};
+//abc();
+
+
+let add = function(a, b){
+    return a+b;
+}
+console.log(add);
+console.log(add(4, 6));
+console.log(add());
+
