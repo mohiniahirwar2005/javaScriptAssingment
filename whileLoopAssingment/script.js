befa7176ab203aa0db9313049dc22306277c1fe5
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-05-01 17:02:32 +0530

    start from 2, keep adding 2 while number < 11

--- whileLoopAssingment/script.js
+++ whileLoopAssingment/script.js
@@ -4,11 +4,20 @@ while(i1 <= 5) {
   console.log(i1);
   i1++;
 }
+//quetion number 2
 
-
-// 2. Print numbers from 5 to 1
 let i2 = 5;
 while(i2 >= 1) {
   console.log(i2);
   i2--;
 }
+
+
+
+// 3. Start from 2, keep adding 2 while number < 11
+let i3 = 2;
+while(i3 < 11) {
+  console.log(i3);
+  i3 += 2;
+}
+
