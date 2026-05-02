8e1eec197bf0e9a374961a459e407c2f63ba3c73
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-05-02 16:47:07 +0530

    Write a program to check if a number is even or odd

--- javaScriptIfElseAssingment/script.js
+++ javaScriptIfElseAssingment/script.js
@@ -1,3 +1,4 @@
+//Q no 2
 let num = 15;
 
 if (num > 10) {
@@ -5,3 +6,15 @@ if (num > 10) {
 } else {
   console.log("Smaller");
 }
+
+//q no 2
+let num1 = 8;
+
+if (num1 % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+
+
