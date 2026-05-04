526a6336e882d7ce2b5464852eb5583eed7c1b03
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-05-04 17:24:10 +0530

    Switch case Assingment

---
+++ switchCaseAssingment/script.js
@@ -0,0 +1,171 @@
+//quetion number 1
+let choice = 2;
+
+switch (choice) {
+  case 1:
+    console.log("Tea");
+    break;
+  case 2:
+    console.log("Coffee");
+    break;
+  case 3:
+    console.log("Juice");
+    break;
+  default:
+    console.log("Invalid");
+}
+
+//quetion number 2
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+}
+
+//quetion number 3
+let flavor = "Chocolate";
+
+switch (flavor) {
+  case "Vanilla":
+    console.log("Vanilla selected");
+    break;
+  case "Chocolate":
+    console.log("Chocolate selected");
+    break;
+  case "Strawberry":
+    console.log("Strawberry selected");
+    break;
+  default:
+    console.log("Unknown flavor");
+}
+
+//quetion number 4
+let num = 10;
+
+switch (num) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  default:
+    console.log("No match found");
+}
+
+//quetion number 5
+let x = 1;
+
+switch (x) {
+  case 1:
+    console.log("Case 1 executed");
+    break;
+  case 2:
+    console.log("Case 2 executed");
+    break;
+}
+
+//quetion number 6
+let x = 1;
+
+switch (x) {
+  case 1:
+    console.log("Case 1");
+  case 2:
+    console.log("Case 2");
+  default:
+    console.log("Default");
+}
+
+//quetion number 7
+let grade = "B";
+
+switch (grade) {
+  case "A":
+  case "B":
+  case "C":
+    console.log("Pass");
+    break;
+  default:
+    console.log("Fail");
+}
+
+//quetion number 8
+let menu = "Home";
+
+switch (menu) {
+  case "Home":
+    console.log("Welcome to Home Page");
+    break;
+  case "About":
+    console.log("About Us Page");
+    break;
+  case "Contact":
+    console.log("Contact Page");
+    break;
+  default:
+    console.log("Page not found");
+}
+
+//quetion number 9
+let value = "1";
+
+switch (value) {
+  case 1:
+    console.log("Number 1");
+    break;
+  case "1":
+    console.log("String 1");
+    break;
+}
+
+//quetion number 10
+let num = 2;
+
+switch (num) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+}
+
+//quetion number 11
+let color = "Blue";
+
+switch (color) {
+  case "Red":
+    console.log("Red color");
+    break;
+  case "Green":
+    console.log("Green color");
+    break;
+  default:
+    console.log("Other color");
+}
