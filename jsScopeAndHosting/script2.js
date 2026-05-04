aec628f45718f4346e3becb9faa6cfbe4b852cbd
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-05-04 16:40:50 +0530

    Scope and hosting

---
+++ jsScopeAndHosting/script2.js
@@ -0,0 +1,35 @@
+"use Strict";
+
+let name = "M";
+console.log(name);
+
+
+
+
+let a;
+let b;
+let c;
+
+a = 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+b = 15;
+console.log(a);
+console.log(b);
+console.log(c);
+
+c= 20;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+abc();
+
+function abc(){
+    console.log("I am  a Function");
+    
+}
