872b1919c7e89a7d2dce4a76b17f9a59cbb8096a
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-05-05 12:15:40 +0530

    Assingment

--- JsDOM/script.js
+++ JsDOM/script.js
@@ -1,11 +1,10 @@
 function submit() {
-  console.log("Sumit Button Clicked");
+     console.log("Sumit Button Clicked"); 
+     const fn = document.getElementById("fullName").value; 
+     console.log(fn); document.getElementById("myData").innerText = fn; 
+     document.getElementById("myData2").innerText = fn; 
+     document.getElementById("fullName").value = ""; 
+     document.getElementById("dataCard").classList.add("divShow") 
+     document.getElementById("dataCard").classList.remove("divHide") 
 
-  const fn = document.getElementById("fullName").value;
-  console.log(fn);
-
-  document.getElementById("myData").innerText = fn;
-  document.getElementById("fullName").value = "";
 }
-
-
