57755e976e1a1f475dcffb15d8c24ca8d37641f2
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-05-08 18:34:21 +0530

    javascript , onclick

--- JsEvenHandeling/script.js
+++ JsEvenHandeling/script.js
@@ -1,18 +1,46 @@
-function Bulb1ON(){
-    document.getElementById("bulb1").style.backgroundColor="yellow";
+/*function Bulb1ON() {
+    document.getElementById("bulb1").style.backgroundColor = "yellow";
 }
 
-
-function Bulb1OFF(){
+function Bulb1OFF() {
     document.getElementById("bulb1").style.backgroundColor = "white";
 }
 
-
-function Bulb1Red(){
-    document.getElementById("bulb1").style.backgroundColor="red";
+function Bulb1Red() {
+    document.getElementById("bulb1").style.backgroundColor = "red";
 }
 
-function Bulb1Green(){
-    document.getElementById("bulb1").style.backgroundColor="green";
+function Bulb1Blue() {
+    document.getElementById("bulb1").style.backgroundColor = "blue";
 }
 
+function Bulb1Green() {
+    document.getElementById("bulb1").style.backgroundColor = "green";
+}
+
+
+
+document.getElementById("bulbRed").addEventListener("click", Bulb1Red);
+
+document.getElementById("bulbBlue").addEventListener("click", Bulb1Blue);
+
+document.getElementById("bulbGreen").addEventListener("click", Bulb1Green);
+
+
+
+document.getElementById("bulb1Color").addEventListener("change", ChangeBulbColor);
+
+function ChangeBulbColor() {
+
+    const color = document.getElementById("bulb1Color").value;
+
+  
+    document.getElementById("abcd").style.backgroundColor = color;
+}
+
+
+function reset(){
+    window.location.reload();
+}
+
+*/
