90beb69b53a5e5fa3188ae6a1f637cb210fb8527
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-06-06 19:07:28 +0530

    react

--- frontend/myFirstReactApp/src/pages/Login.jsx
+++ frontend/myFirstReactApp/src/pages/Login.jsx
@@ -1,8 +1,19 @@
 function Login(){
     return (
         <>
-        <div className="container d-flex justify-content-center align-items-center">
-            <span className="text-primary fs-2">Login</span>
+        <div className="d-flex justify-content-center">
+            <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+                <h1 className="tex-center">Welcome Back!!!</h1>
+                <br />
+                <form>
+                    <label htmlFor="userName">Username</label>
+                    <input type="text" name="userName" className="form-control"/>
+                    <br />
+                    <label htmlFor="passward">Passward</label>
+                    <input type="text" name="passward" className="form-control"/>
+                </form>
+            </div>
+            
         </div>
         </>
     );
